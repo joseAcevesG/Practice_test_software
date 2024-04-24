76b0fe662d15db4994ecac032b44c2d214c6b0a1
joseAcevesG <119601853+joseAcevesG@users.noreply.github.com>
2024-04-24 23:37:55 +0000

    feat: mock-up testing homework
    homework to lean hoe to do mock testing

---
+++ src/subprocess.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def execute_command(command):
+    try:
+        result = subprocess.run(command, capture_output=True, text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        raise e
